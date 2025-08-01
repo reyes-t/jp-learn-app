50a4d13990e1fd5cc3e9a28047c8e6c3a79de7c7
TED OWEN B. REYES <190315496+reyes-t@users.noreply.github.com>
2025-08-01 03:22:42 +0000

    center the "loading..." text that is displayed whenever the app reloads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ function AppContent({ children }: { children: React.ReactNode }) {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center h-screen w-screen">
         <p>Loading...</p>
       </div>
     );
